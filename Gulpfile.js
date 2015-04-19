edf69eafa78baebede65846d05cd465383476ec5
Ben Van Treese <vantreeseba@gmail.com>
2015-04-18 22:41:26 -0500

    Clean up travis yml and gulp

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,38 +3,23 @@ var gulp = require('gulp');
 var shell = require('gulp-shell');
 var mkdirp = require('mkdirp');
 
-var watching = false;
-
-gulp.on('err', function errHandler(err) {
-    process.emit('exit', err.err);
-});
-
-process.on('exit', function(err) {
-    process.nextTick(function() {
-		if(!watching){
-        	process.exit(err.code);
-		}
-    });
-});
-
-gulp.task('create-artifacts-dir', function() {
+function createArtifactDirs() {
     mkdirp('artifacts/js');
     mkdirp('artifacts/test');
-});
+}
 
+function watch() {
+    var watchDirs = ['src/**/*.hx', 'test/**/*.hx', 'targets/**'];
+    gulp.watch(watchDirs, ['test']);
+}
+
+gulp.task('create-artifacts-dir', createArtifactDirs);
 gulp.task('build-js', shell.task('haxe targets/js.hxml'));
+gulp.task('test', shell.task('haxelib run munit test -result-exit-code'));
+gulp.task('test-coverage', shell.task('haxelib run munit test -coverage -result-exit-code'));
+
+gulp.task('watch', watch);
 
 gulp.task('build', ['create-artifacts-dir', 'build-js']);
-
-gulp.task('test', function() {
-    return gulp
-        .src('')
-        .pipe(shell('haxelib run munit test -result-exit-code'));
-});
-
-gulp.task('test-coverage', shell.task('haxelib run munit test -coverage'));
-
-gulp.task('watch', function() {
-	watching = true;
-    gulp.watch(['src/**/*.hx', 'test/**/*.hx', 'targets/**'], ['test']);
-});
+gulp.task('ci', ['test-coverage', 'build']);
+gulp.task('default', ['ci']);
