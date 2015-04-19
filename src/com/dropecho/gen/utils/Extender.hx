5aa2a2a2914599904bedf6c69550f9a14b5d4988
Ben Van Treese <vantreeseba@gmail.com>
2015-04-18 22:56:26 -0500

    Add javascript testing Fix issue in extender under js

--- src/com/dropecho/gen/utils/Extender.hx
+++ src/com/dropecho/gen/utils/Extender.hx
@@ -1,26 +1,27 @@
 package com.dropecho.gen.utils;
 
 class Extender {
-    public static function extend(base:Dynamic, extension:Dynamic){
-        for(ff in Reflect.fields(extension)){
+	public static function extend(base:Dynamic, extension:Dynamic){
+		if(base == null){
+			base = {};
+		}
+		for(ff in Reflect.fields(extension)){
 
-            if(!Reflect.hasField(base,ff)){
-                continue;
-            }
+			if(!Reflect.hasField(base,ff)){
+				continue;
+			}
 
-            var exField = Reflect.field(extension, ff);
-            var baseField = Reflect.field(base,ff);
+			var exField = Reflect.field(extension, ff);
+			var baseField = Reflect.field(base,ff);
 
-            if(Reflect.isObject(baseField)){
-                if(Reflect.isObject(exField)){
-                    extend(baseField, exField);
-                }
-                continue;
-            }
+			if(Reflect.isObject(baseField)){
+				if(Reflect.isObject(exField)){
+					extend(baseField, exField);
+				}
+				continue;
+			}
 
-            Reflect.setField(base, ff, exField);
-
-
-        }
-    }
+			Reflect.setField(base, ff, exField);
+		}
+	}
 }
