259aa0a5d5a493c303d0d1df3f25f11b02fc8ee9
Ben Van Treese <vantreeseba@gmail.com>
2015-05-02 03:48:04 -0500

    Fix some generation problems.
    Move check to shortcut out of splitting to top of makesplit.
    Add minheight check to select vertical or horizontal split.

--- src/com/dropecho/gen/bsp/Generator.hx
+++ src/com/dropecho/gen/bsp/Generator.hx
@@ -4,66 +4,70 @@ import com.dropecho.gen.utils.Extender;
 
 @:expose("degen.bspGenerator")
 class Generator {
-    public var width:Int = 256;
-    public var height:Int = 256;
-    public var minHeight:Int = 10;
-    public var minWidth:Int = 10;
-    public var depth:Int = 10;
-    public var ratio:Float = .45;
-    public var x:Int = 0;
-    public var y:Int = 0;
+	public var width:Int = 256;
+	public var height:Int = 256;
+	public var minHeight:Int = 10;
+	public var minWidth:Int = 10;
+	public var depth:Int = 10;
+	public var ratio:Float = .45;
+	public var x:Int = 0;
+	public var y:Int = 0;
 
-    public function new(?ops:Dynamic){
-        Extender.extend(this, ops);
-    }
+	public function new(?ops:Dynamic){
+		Extender.extend(this, ops);
+	}
 
-    public function generate(root:Node = null):Node{
-        if(root == null){
-            root = new Node({height: height, width: width, x: x, y: y});
-        }
-        buildTree(root);
+	public function generate(root:Node = null):Node{
+		if(root == null){
+			root = new Node({height: height, width: width, x: x, y: y});
+		}
+		buildTree(root);
 
-        return root;
-    }
+		return root;
+	}
 
-    private function buildTree(node:Node, ?level:Int = 0):Void{
-        if(node == null || level >= depth){
-            return;
-        }
+	private function buildTree(node:Node, ?level:Int = 0):Void{
+		if(node == null || level >= depth){
+			return;
+		}
 
-        makeSplit(node);
-        buildTree(node.left, level + 1);
-        buildTree(node.right, level + 1);
-    }
+		makeSplit(node);
+		buildTree(node.left, level + 1);
+		buildTree(node.right, level + 1);
+	}
 
-    private function makeSplit(node:Node):Void{
-        var splitAt = 0;
-        var vertical = Std.random(2) == 1;
+	private function makeSplit(node:Node):Void{
 
-        if(node.width > node.height * ratio){
-            vertical = false;
-        } else if(node.height > node.width * ratio){
-            vertical = true;
-        } else {
-            return;
-        }
+		//Times two because you need to fit two child nodes into this one.
+		if(node.width < minWidth * 2 && node.height < minHeight * 2){
+			return;
+		}
 
-        if(node.width < minWidth * 2 || node.height < minHeight * 2){
-            return;
-        }
+		var splitAt = 0;
+		var vertical : Bool;
 
-        if(vertical){
-            splitAt = Std.random(node.height - (minHeight * 2)) + minHeight;
-            var rHeight = node.height - splitAt;
+		if(node.height < 2 * minHeight || node.width > node.height * ratio){
+			vertical = false;
+		} else if(node.width < 2 * minWidth || node.height > node.width * ratio){
+			vertical = true;
+		} else {
+			return;
+		}
 
-            node.left = new Node({height: splitAt, width: node.width, parent: node, x: node.x, y: node.y});
-            node.right = new Node({height: rHeight, width: node.width, parent: node, x: node.x, y: node.y + splitAt});
-        } else {
-            splitAt = Std.random(node.width - (minWidth * 2)) + minWidth;
-            var rWidth = node.width - splitAt;
+		if(vertical){
+			splitAt = Std.random(node.height - (minHeight * 2)) + minHeight;
+			var rHeight = node.height - splitAt;
 
-            node.left = new Node({height: node.height, width: splitAt, parent: node, x: node.x, y: node.y});
-            node.right = new Node({height: node.height, width: rWidth, parent: node, x: node.x + splitAt, y: node.y});
-        }
-    }
+			node.left = new Node({height: splitAt, width: node.width, parent: node, x: node.x, y: node.y});
+			node.right = new Node({height: rHeight, width: node.width, parent: node, x: node.x, y: node.y + splitAt});
+		} else {
+			splitAt = Std.random(node.width - (minWidth * 2)) + minWidth;
+			var rWidth = node.width - splitAt;
+
+			node.left = new Node({height: node.height, width: splitAt, parent: node, x: node.x, y: node.y});
+			node.right = new Node({height: node.height, width: rWidth, parent: node, x: node.x + splitAt, y: node.y});
+		}
+
+		return;
+	}
 }
--- test/bsp/GeneratorTest.hx
+++ test/bsp/GeneratorTest.hx
@@ -20,44 +20,105 @@ class GeneratorTest {
     }
 
 	@Test
-    public function test_given_a_depth_of_1_it_should_generate_a_single_level_tree(){
-        generator.depth = 1;
-        var root = generator.generate();
+	public function test_given_a_depth_of_1_it_should_generate_a_single_level_tree(){
+		generator.depth = 1;
+		var root = generator.generate();
 
-        Assert.isFalse(root.isLeaf());
-        Assert.isTrue(root.right.isLeaf());
-        Assert.isTrue(root.left.isLeaf());
-    }
-
-	@Test
-    public function test_given_a_min_height_every_generated_node_should_have_a_greater_height(){
-        generator.depth = 500;
-        generator.minHeight = 10;
-        var root = generator.generate();
-
-        root.traverseInOrder(function(node){
-            Assert.isTrue(node.height >= 10);
-        });
-    }
+		Assert.isFalse(root.isLeaf());
+		Assert.isTrue(root.right.isLeaf());
+		Assert.isTrue(root.left.isLeaf());
+	}
 
 	@Test
-    public function test_given_a_min_width_every_generated_node_should_have_a_greater_width(){
-        generator.depth = 500;
-        generator.minWidth = 10;
-        var root = generator.generate();
+	public function test_given_a_min_height_every_generated_node_should_have_a_greater_height(){
+		generator.depth = 50;
+		generator.minHeight = 10;
+		generator.ratio = 1;
+		var root = generator.generate();
 
-        root.traverseInOrder(function(node){
-            Assert.isTrue(node.width >= 10);
-        });
-    }
+		root.traverseInOrder(function(node){
+			Assert.isTrue(node.height >= 10);
+		});
+	}
 
 	@Test
-    public function test_given_a_min_width_every_generated_node_should_have_a_ratio_greater_than_given(){
-        generator.depth = 4;
-        var root = generator.generate();
+	public function test_given_a_min_width_every_generated_node_should_have_a_greater_width(){
+		generator.depth = 50;
+		generator.minWidth = 10;
+		generator.ratio = 1;
+		var root = generator.generate();
 
-        root.traverseInOrder(function(node){
-            Assert.isTrue(node.width/node.height >= generator.ratio || node.height/node.width >= generator.ratio);
-        });
-    }
+		root.traverseInOrder(function(node){
+			Assert.isTrue(node.width >= 10);
+		});
+	}
+
+	@Test
+	public function test_given_a_min_width_every_generated_node_should_have_a_ratio_greater_than_given(){
+		generator.depth = 4;
+		var root = generator.generate();
+
+		root.traverseInOrder(function(node){
+			Assert.isTrue(node.width/node.height >= generator.ratio || node.height/node.width >= generator.ratio);
+		});
+	}
+
+	@Test
+	public function given_a_tree_every_set_of_child_nodes_should_add_up_to_the_height_and_width_of_their_parents(){
+		for(i in 0...100){
+			generator.depth = 4;
+			var root = generator.generate();
+			root.traverseInOrder(function(node){
+				if(!node.isLeaf()){
+					//vertical split
+					if(node.left.height == node.height){
+						var childWidths = node.left.width + node.right.width;
+
+						Assert.areEqual(node.width, childWidths);
+						Assert.areEqual(node.height, node.right.height);
+						Assert.areEqual(node.height, node.left.height);
+					}
+					else {
+						var childHeights = node.left.height + node.right.height;
+						Assert.areEqual(node.height, childHeights);
+						Assert.areEqual(node.width, node.left.width);
+						Assert.areEqual(node.width, node.right.width);
+					}
+				}
+			});
+		}
+	}
+
+	@Test
+	public function given_a_tree_every_leafs_area_should_add_up_to_root_area(){
+		for(i in 0...100){
+			generator.depth = 4;
+			var root = generator.generate();
+			var rootArea = root.height * root.width;
+			var leafArea = 0;
+
+			root.traverseInOrder(function(node){
+				if(node.isLeaf()){
+					leafArea += node.height * node.width;
+				}
+			});
+
+			Assert.areEqual(rootArea, leafArea);
+		}
+	}
+
+	@Test
+	public function given_a_tree_every_non_leaf_should_have_2_children(){
+		for(i in 0...100){
+			generator.depth = 4;
+			var root = generator.generate();
+
+			root.traverseInOrder(function(node){
+				if(!node.isLeaf()){
+					Assert.isTrue(node.left != null);
+					Assert.isTrue(node.right != null);
+				}
+			});
+		}
+	}
 }
