b5bf8286a567c113437ca27309095b1cbcae4db6
Benjamin Van Treese <vantreeseba@gmail.com>
2016-04-01 00:56:08 -0500

    adding bsp test

--- test/export/TiledExporterTest.hx
+++ test/export/TiledExporterTest.hx
@@ -1,6 +1,9 @@
 package export;
 
 import massive.munit.Assert;
+import degen.bsp.Generator;
+import degen.bsp.Node;
+import degen.map.generators.RoomGenerator;
 import degen.map.Map2d;
 import degen.ca.Generator as CaGen;
 import degen.export.TiledExporter;
@@ -13,29 +16,43 @@ class TiledExporterTest {
     @Test
     public function ca_map_test() {
 
-        var params = {
-            steps: [{
-                reps: 4,
-                r1_cutoff: 5,
-                r2_cutoff: 2
-            },
-            {
-                reps : 3,
-                r1_cutoff: 5,
-                r2_cutoff: 0
-            }],
-            height: 32,
-            width: 32,
-            tile_floor: 1,
-            tile_wall: 0
+        //var params = {
+            //steps: [{
+                //reps: 4,
+                //r1_cutoff: 5,
+                //r2_cutoff: 2
+            //},
+            //{
+                //reps : 3,
+                //r1_cutoff: 5,
+                //r2_cutoff: 0
+            //}],
+            //height: 32,
+            //width: 32,
+            //tile_floor: 1,
+            //tile_wall: 0
 
-        };
+        //};
 
-        var map = CaGen.generate(params);
+		//var map = CaGen.generate(params);
+			var genOpts = {
+			width: 100,
+			height: 50,
+			minHeight: 10,
+			minWidth: 10,
+			depth: 10,
+			ratio: .65
+			};
 
-        var json = TiledExporter.export('map.json', map);
+			var gen = new Generator(genOpts);
 
-		File.saveContent("map.json", json);	
+		var map = RoomGenerator.buildRooms(gen.generate());
+
+
+
+		var json = TiledExporter.export('map.json', map);
+
+		File.saveContent("bsp.json", json);	
 
         Assert.isTrue(true);
     }
--- test/map/Map2dTest.hx
+++ test/map/Map2dTest.hx
@@ -10,25 +10,25 @@ import degen.ca.Generator as CaGen;
 class Map2dTest {
     var map : Map2d;
 
-    @Before
-        public function setup(){
-            map = new Map2d(100, 50);
-        }
+    //@Before
+        //public function setup(){
+            //map = new Map2d(100, 50);
+        //}
 
     @Test
         public function bspMapTest() {
-            //var genOpts = {
-            //width: 100,
-            //height: 50,
-            //minHeight: 10,
-            //minWidth: 10,
-            //depth: 10,
-            //ratio: .65
-            //};
+			var genOpts = {
+				width: 100,
+				height: 50,
+				minHeight: 10,
+				minWidth: 10,
+				depth: 10,
+				ratio: .65
+			};
 
-            //var gen = new Generator(genOpts);
+			var gen = new Generator(genOpts);
 
-            //trace(RoomGenerator.buildRooms(gen.generate()));
+			trace(RoomGenerator.buildRooms(gen.generate()));
 
 
             //var params = {
