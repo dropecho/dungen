af995fcf41d78f863d7efe87a1dcc7fc98349471
Benjamin Van Treese <vantreeseba@gmail.com>
2020-04-24 21:20:18 -0500

    Clean up

--- test/TestSuite.hx
+++ test/TestSuite.hx
@@ -12,10 +12,8 @@ import export.TiledExporterTest;
  * Auto generated Test Suite for MassiveUnit.
  * Refer to munit command line tool for more information (haxelib run munit)
  */
-
 class TestSuite extends massive.munit.TestSuite
-{		
-
+{
 	public function new()
 	{
 		super();
